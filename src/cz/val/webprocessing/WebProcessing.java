db05c58605ed4d265dbf3b00ce59a9f459472260
val0065 <david.vala@valada.cz>
2018-04-15 22:45:14 +0200

    Process progress

--- src/cz/val/webprocessing/WebProcessing.java
+++ src/cz/val/webprocessing/WebProcessing.java
@@ -10,8 +10,8 @@ public class WebProcessing {
         Process proc = new Process();
 
         //System.out.println(proc.lengthOfLine());
-        //System.out.println(proc.lengthOfLineWithFilter());
-        System.out.println(proc.overlayPolygons());
+        System.out.println(proc.lengthOfLineWithFilter());
+        //System.out.println(proc.overlayPolygons());
         //System.out.println(proc.overlayPolygonsWithFilter());
 
     }
